f2d0b8d44c5d4bc2e94d039c5281daa684365636
Nathan <nathanpykimutai@gmail.com>
2020-12-01 11:45:09 +0300

    created the ingredient model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -51,3 +51,11 @@ class ModelTest(TestCase):
         )
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_ingredients_str(self):
+        """Test the ingredient string repr"""
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(),
+            name="Cucumber"
+        )
+        self.assertEqual(str(ingredient), ingredient.name)
